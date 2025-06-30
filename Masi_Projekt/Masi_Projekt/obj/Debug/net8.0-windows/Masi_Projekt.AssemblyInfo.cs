6eaef2d02f8bc737d442692200279b82b1180a38
wardusV2 <wardusandre@gmail.com>
2025-06-30 22:34:38 +0200

    fix

--- Masi_Projekt/Masi_Projekt/obj/Debug/net8.0-windows/Masi_Projekt.AssemblyInfo.cs
+++ Masi_Projekt/Masi_Projekt/obj/Debug/net8.0-windows/Masi_Projekt.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Masi_Projekt")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+17d091929904fe158fb63d0157615a6f93c3c5cd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f1f54ee9f4422e5230bc1a7dfbe82755693435a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Masi_Projekt")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Masi_Projekt")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
